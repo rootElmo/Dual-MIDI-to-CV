6f87076c4b45029976179c1ba473631261c3315c
rootElmo <elmo.rohula@gmail.com>
2021-01-18 13:18:15 +0200

    Fix delays in blink and add notes

--- Midivoks.ino
+++ Midivoks.ino
@@ -24,8 +24,7 @@ void setup(){
   midiIn.begin();
   Serial.begin(9600);
 
-  blink();
-  Serial.println("Tämä on setupissa oleva println");
+  Serial.println("Setup println, I provide no actual value to debugging");
   
   notesActive = 0;
 }
@@ -35,8 +34,9 @@ void loop() {
 }
 
 void noteOn(byte channel, byte pitch, byte vel){
-  Serial.print("Nuotti: ");
-  Serial.println(pitch);
+  Serial.print("Note: ");
+  Serial.print(pitch);
+  Serial.print(", ");
   if (vel <= 0) {
     // noteOff
     return;
@@ -45,16 +45,16 @@ void noteOn(byte channel, byte pitch, byte vel){
       case 0:
         // new note -> 1st note
         // notesActive == 1
-        Serial.println("Eka nuotti päällä!");
+        Serial.println("First note on");
         notesActive = 1;
-        blink();
+        blinkOn();
         break;
       case 1:
         // 1st note -> 2nd note
         // new note -> 1st note
         // notesActive = 2
-        Serial.println("Toka nuotti päällä!");
-        blink();
+        Serial.println("Second note on");
+        blinkOn();
         notesActive = 2;
         break;
       case 2:
@@ -62,37 +62,37 @@ void noteOn(byte channel, byte pitch, byte vel){
         // 1st note -> 2nd note
         // new note -> 1st note
         // no change to 'notesActive'
-        blink();
-        Serial.println("Maksimimäärä nuotteja päällä!");
+        blinkOn();
+        Serial.println("Maximum number of notes on");
         break;
     }
   }
 }
 
-void blink(){
-  int loops = 3;
-  for (int i = 0; i < loops; i++) {
-      digitalWrite(LEDboard, HIGH);
-      delay(200);
-      digitalWrite(LEDboard, LOW);
-      delay(200);
-    }
+void blinkOn(){
+    digitalWrite(LEDboard, HIGH);
+  }
+
+void blinkOff(){
+    digitalWrite(LEDboard, LOW);
   }
 
 void noteOff(byte channel, byte pitch, byte vel){
   switch(notesActive) {
       case 0:
         break;
-        Serial.println("Ei nuotteja pois päältä");
+        Serial.println("No notes on");
       case 1:
         // simply turn off note
-        Serial.println("Eka nuotti pois päältä!");
+        Serial.println("First note off");
         notesActive = 0;
+        blinkOff();
         break;
       case 2:
         // turn off 2nd note
-        Serial.println("Toka nuotti pois päältä!");
+        Serial.println("Second note off");
         notesActive = 1;
+        blinkOff();
         break;
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# MIDIVOKS
+# Dual MIDI to CV
+
+A simple circuit to get 2 CV outs from MIDI.
+
+**UNDER CONSTRUCTION**
+
+## To-do
+
+**Note on/off logic**
+
+Assign variables/flags to notes.
+
+  * If both notes are pressed, and the either is released, the released note should turn off, not the one which was on first.
+  * Polivoks-style note priority logic; if both notes are on, and a new note is pressed which is higher than the lowest note -> lowest note stays on, higher note gets changed to new note
