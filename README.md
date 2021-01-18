cc6da2fc2fbd7a54f9c1c6f9dfec3b21e39d470e
rootElmo <elmo.rohula@gmail.com>
2021-01-18 16:21:03 +0200

    Finish note priority logic, add notes

--- README.md
+++ README.md
@@ -1,14 +1,42 @@
 # Dual MIDI to CV
 
-A simple circuit to get 2 CV outs from MIDI.
+A simple circuit to get 2 CV outs from MIDI. CV will be scaled to 1V/Oct.
 
 **UNDER CONSTRUCTION**
 
 ## To-do
 
-**Note on/off logic**
+Here are the features planned for the module.
+
+**Add schematics (Not started)**
+
+**Note on/off logic (Done as of 18.1.2021)** 
 
 Assign variables/flags to notes.
 
   * If both notes are pressed, and the either is released, the released note should turn off, not the one which was on first.
-  * Polivoks-style note priority logic; if both notes are on, and a new note is pressed which is higher than the lowest note -> lowest note stays on, higher note gets changed to new note
+  * Polivoks-style note priority logic; if both notes are on, and a new note is pressed which is higher than the lowest note -> lowest note stays on, higher note gets changed to new note.
+
+**Gate signals for _Note On_ and _Note Off_ messages (Not started)**
+
+  * Simple **DigitalWrite** should be enough. Voltages can be amplified with op-amps.
+
+**Make functions to send _Note_ values to _DAC_ (Not started)** 
+  
+  * **DAC** will be either 2 or 4 channel, **12-bit**.
+  * Need to learn basic usage of a **DAC**.
+
+**Scale and trim _DAC_ output to match _1v/Oct-scaling_ (Not started)**
+
+**Implement pitchbend (Not started)**
+
+  * Math is required (yuck).
+
+**Add trigger outputs (POSSIBLE FEATURE)**
+
+  * Not mandatory, but could be helpful for some situations.
+
+**Add _velocity_ output (POSSIBLE FEATURE)**
+
+
+Made by: Elmo Rohula
--- dual_midi_to_cv.ino
+++ dual_midi_to_cv.ino
@@ -6,6 +6,22 @@
 // version 0.0.1
 // A Dual MIDI IN device
 
+
+// BUGS:
+
+// FIXED BUGS:
+// - 1. When two notes are active and top one is released
+// - both notes don't revert to bottom note's value.
+// - Program doesn't recognice note off ???
+//
+// ----> in "Note Off" switch, case "2", it checked if the
+// MIDI is not equal to top OR bottom note.
+// Since either was always true, since Note Off
+// was received only by one note, the program would misbehave.
+// Now it checks if the MIDI
+// Note Off is not equal to top note AND bottom note.
+// DATE: 18.1.2021
+
 #include <MIDI.h>
 
 #define LEDboard 13
@@ -15,7 +31,7 @@ MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, midiIn);
 byte data;
 bool noteActive;
 bool maxNotes;
-int notesActive;
+int notesActive, topNote, bottomNote;
 
 void setup(){
   pinMode(LEDboard, OUTPUT);
@@ -25,7 +41,9 @@ void setup(){
   Serial.begin(9600);
 
   Serial.println("Setup println, I provide no actual value to debugging");
-  
+
+  bottomNote = 0;
+  topNote = 0;
   notesActive = 0;
 }
 
@@ -34,39 +52,130 @@ void loop() {
 }
 
 void noteOn(byte channel, byte pitch, byte vel){
-  Serial.print("Note: ");
+  Serial.print("MIDI NOTE ON: channel: ");
+  Serial.print(channel);
+  Serial.print(", pitch: ");
   Serial.print(pitch);
-  Serial.print(", ");
+  Serial.print(", velocity: ");
+  Serial.println(vel);
+
+  // Function called, when "Note On" received from MIDI
+  
   if (vel <= 0) {
     // noteOff
     return;
   } else {
     switch(notesActive){
       case 0:
-        // new note -> 1st note
-        // notesActive == 1
-        Serial.println("First note on");
+        // new note -> both notes playing: monophonic
+        
+        topNote = pitch;
+        bottomNote = pitch;
+        
         notesActive = 1;
         blinkOn();
+        printNotes(topNote, bottomNote, notesActive);
         break;
       case 1:
-        // 1st note -> 2nd note
-        // new note -> 1st note
-        // notesActive = 2
-        Serial.println("Second note on");
-        blinkOn();
-        notesActive = 2;
-        break;
+
+        if (pitch == bottomNote) { // no changes, stays monophonic
+          // (possible feature?) -> RE-TRIGGER both top and bottom note
+          // requires module to have trigger outputs
+          
+          blinkOn();
+          break;  
+        } else if (pitch > bottomNote) { // new note > old note -> duophonic: top note gets new value
+          topNote = pitch;
+          notesActive = 2;
+          
+          blinkOn();
+          printNotes(topNote, bottomNote, notesActive);
+          break;
+        } else {  // new note < old note -> duophonic: bottom note gets new value
+          bottomNote = pitch;
+          notesActive = 2;
+
+          printNotes(topNote, bottomNote, notesActive);
+          blinkOn();
+          break;
+        }
+
       case 2:
-        // replace note flagged 2nd note ->
-        // 1st note -> 2nd note
-        // new note -> 1st note
-        // no change to 'notesActive'
-        blinkOn();
-        Serial.println("Maximum number of notes on");
-        break;
+
+        if (pitch == topNote || pitch == bottomNote) { // no changes, stays duophonic
+            // (possible feature?) -> RE-TRIGGER
+            // requires module to have trigger outputs
+
+            blinkOn();
+            break;
+          } else if (pitch > bottomNote) { // new note > bottom note -> change top note
+            topNote = pitch;
+
+            printNotes(topNote, bottomNote, notesActive);
+
+            blinkOn();
+            break;
+          } else if (pitch < bottomNote) { // new note < bottom note -> change bottom note
+            bottomNote = pitch;
+
+            printNotes(topNote, bottomNote, notesActive);
+
+            blinkOn();
+            break;
+          }
     }
   }
+}
+
+void noteOff(byte channel, byte pitch, byte vel){
+  Serial.print("MIDI NOTE OFF: channel: ");
+  Serial.print(channel);
+  Serial.print(", pitch: ");
+  Serial.print(pitch);
+  Serial.print(", velocity: ");
+  Serial.println(vel);
+  // Function called, when "Note Off" received from MIDI
+  
+  switch(notesActive) {
+      case 0:
+        break;
+      case 1:
+
+        if (pitch != topNote || pitch != bottomNote) {
+          
+          break;
+        } else if (pitch == topNote || pitch == bottomNote) {
+          notesActive = 0;
+          topNote = 0;
+          bottomNote = 0;
+  
+          printNotes(topNote, bottomNote, notesActive);
+          
+          blinkOff();
+          break;
+        }
+        
+      case 2:
+
+        if (pitch != bottomNote && pitch != topNote) { // Note turned off isn't either active one
+          
+          break;
+        } else if (pitch == bottomNote) { // bottom note is turned off -> monophonic
+          bottomNote = topNote;
+          notesActive = 1;
+
+          printNotes(topNote, bottomNote, notesActive);
+
+          break;
+        } else if (pitch == topNote) { // bottom note is turned off -> monophonic
+          topNote = bottomNote;
+          notesActive = 1;
+
+          printNotes(topNote, bottomNote, notesActive);
+
+          break;
+        }
+    }
 }
 
 void blinkOn(){
@@ -77,22 +186,11 @@ void blinkOff(){
     digitalWrite(LEDboard, LOW);
   }
 
-void noteOff(byte channel, byte pitch, byte vel){
-  switch(notesActive) {
-      case 0:
-        break;
-        Serial.println("No notes on");
-      case 1:
-        // simply turn off note
-        Serial.println("First note off");
-        notesActive = 0;
-        blinkOff();
-        break;
-      case 2:
-        // turn off 2nd note
-        Serial.println("Second note off");
-        notesActive = 1;
-        blinkOff();
-        break;
-    }
-}
+void printNotes(int n1, int n2, int nActive) {
+    Serial.print("TOP NOTE: ");
+    Serial.print(n1);
+    Serial.print(" || BOTTOM NOTE: ");
+    Serial.print(n2);
+    Serial.print(" || NOTES ACTIVE: ");
+    Serial.println(nActive);
+  }
