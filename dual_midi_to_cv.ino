280f056124c388fa70616810a9f504c5385b5847
rootElmo <elmo.rohula@gmail.com>
2021-01-23 22:12:37 +0200

    Clean up the code

--- dual_midi_to_cv.ino
+++ dual_midi_to_cv.ino
@@ -14,6 +14,7 @@
 
 #define LEDboard 13
 #define gateLED 12
+#define maxNotes 87
 
 mcp4728 myDac = mcp4728(0);
 
@@ -24,12 +25,9 @@ MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, midiIn); // Creates a custom insta
  */
 
 byte data;
-bool maxNotes;
-int notesActive, topNote, bottomNote;
-int notesListIndex = 0;
-int notesList[22] = {0};
+int topNote;
 
-bool noteActive[128] = {0};
+bool notesList[128] = {0};
 
 void setup(){
     pinMode(LEDboard, OUTPUT);
@@ -41,9 +39,7 @@ void setup(){
     myDac.vdd(5000);
     Serial.begin(9600);
     
-    notesActive = 0;
     topNote = 0;
-    bottomNote = 0;
 }
 
 void loop() {
@@ -60,10 +56,13 @@ void noteOn(byte channel, byte pitch, byte vel){
     // Function called, when "Note On" received from MIDI
     
     if (vel <= 0) {
-        noteActive[pitch] = false; // some devices use Note On with 0 velocity as Note Off
+        notesList[pitch] = false; // some devices use Note On with 0 velocity as Note Off
+        return;
+    } else if (pitch < 0 || pitch > maxNotes) {
+        return;
     } else {
-        noteActive[pitch] = true; // set note to active notes list
-        playNote();
+        notesList[pitch] = true; // set note to active notes list
+        handleNote();
     }
   
 }
@@ -77,38 +76,42 @@ void noteOff(byte channel, byte pitch, byte vel){
     Serial.println(vel);
     // Function called, when "Note Off" received from MIDI
 
-    if (noteActive[pitch] != true) {
+    if (notesList[pitch] == false) {
         return;
-    } else {
-        noteActive[pitch] = false;
-        playNote();
+    } else  if (notesList[pitch] == true) {
+        notesList[pitch] = false;
+        digitalWrite(gateLED, LOW);
     }
     
 }
 
-void playNote() {
+void handleNote() {
     // get highest active note and "play" it.
     // Highest pressed note has priority over all other notess
-    topNote = getHighestNote();
-
-    if (topNote == 0) { // if no active notes, set gate off
-        Serial.print("GATE LOW || TOP NOTE: ");
-        Serial.println(topNote);
-        digitalWrite(gateLED, LOW);
-    } else { // if active notes, play highest active
+    topNote = 0;
+    bool noteActive = false;
+    
+    for (int i = 0; i < 128; i++) {
+        if (notesList[i]) {
+            topNote = i;
+            noteActive = true; 
+        }
+    }
+    if (noteActive == true) { // if active notes, play highest active, set gate on
         Serial.print("GATE HIGH || TOP NOTE: ");
         Serial.println(topNote);
         sendToDac(topNote);
         digitalWrite(gateLED, HIGH);
-        
+    } else { // if no active notes, set gate off
+        Serial.print("GATE LOW || TOP NOTE: ");
+        Serial.println(topNote);
+        digitalWrite(gateLED, LOW);
     }
-    /*
-    Serial.print(" || BOTTOM NOTE: ");
-    Serial.println(bottomNote);
-    */
 }
 
+/*
 int getLowestNote() {
+    // DO NOT USE, TO  BE REMOVED
     int min = 128;
     for (int i = 0; i < 128; i++) {
         if (noteActive[i]) {
@@ -121,31 +124,15 @@ int getLowestNote() {
     Serial.println(min);
     return min;
 }
-
-int getHighestNote() {
-    // checks active note list for highest note and returns it
-    int max = 0;
-    for (int i = 0; i < 128; i++) {
-        if (noteActive[i]) {
-            if (i > max) {
-            max = i;  
-            }
-        }
-    }
-    /*
-    Serial.print("MAX: ");
-    Serial.println(max);
-    */
-    return max;
-}
+*/
 
 void sendToDac(int note) {
-    float mV = 85.3125f * note;
-    if (mV >= 4095) {
-        mV = 4095;
+    float mVPerNote = 4095.000f / maxNotes;
+    unsigned int mVToDac = (unsigned int) note * mVPerNote;
+    if (mVToDac >= 4095) {
+        mVToDac = 4095;
     }
-
-    myDac.voutWrite(0, mV);
+    myDac.voutWrite(0, mVToDac);
 }
 
 void blinkOn(){
